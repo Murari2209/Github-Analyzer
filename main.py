f6ef428a7aa53d43e169e93a0dd4a338c4848518
murari2209 <shrivastava.murari@gmail.com>
2026-03-22 01:02:45 +0530

    add feture to main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.api_collector import fetch_repositories
 from src.data_cleaner import clean_data
 from src.analyzer import language_distribution
 from src.visualizer import plot_languages
+from src.analyzer import add_features, rank_repositories
 
 
 df = fetch_repositories()
@@ -11,3 +12,6 @@ clean_df = clean_data("data/raw_repos.csv")
 lang_counts = language_distribution(clean_df)
 
 plot_languages(lang_counts)
+
+featured_df = add_features(clean_df)
+ranked_df = rank_repositories(featured_df)
